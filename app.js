c821aea959fe7e54149e2a0d8468500f1bda55e2
yoroyo <astroarts@hotmail.co.jp>
2020-08-30 03:43:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';   //JavaScript を Strict モードで利用するための記述
+const number = process.argv[2] || 0;
+let sum = 0;
+for (let i = 1; i <= number; i++) {
+    sum = sum + i;
+}
+console.log(sum);
